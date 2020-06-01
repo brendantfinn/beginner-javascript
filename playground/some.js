4ab1294b7e5baf8cd98b0a2ea4c6657c05aeb48f
brendantfinn <brendantfinn@gmail.com>
2020-06-01 14:16:25 -0500

    types - introduction: completed

--- playground/some.js
+++ playground/some.js
@@ -3,3 +3,16 @@ console.log(`some.js - i am in another file`);
 
 const age = 100;
 console.log(`some.js`, p, age);
+
+//TYPES: SEVEN TYPES. Anytime you have a value (varaible, passed to function, etc) fallingto 1/7 of the types 
+// SNOB'N'US
+
+// PRIMATIVE TYPES (1-6)
+// String - text
+// Number - number
+// Boolean - true or false
+// Null - set to "nothing" by dev
+// Undefined - nothing - never defined
+// Symbol - guranteed unique identifier
+
+// Object - everything in JS is a {Object}
