c753e5cb12bca2928e1335e6e1a0edd3ba707e52
brendantfinn <brendantfinn@gmail.com>
2020-06-02 13:12:12 -0500

    eslint/prettier clean up

--- playground/some.js
+++ playground/some.js
@@ -4,7 +4,7 @@ console.log(`some.js - i am in another file`);
 const age = 100;
 console.log(`some.js`, p, age);
 
-//TYPES: SEVEN TYPES. Anytime you have a value (varaible, passed to function, etc) fallingto 1/7 of the types 
+// TYPES: SEVEN TYPES. Anytime you have a value (varaible, passed to function, etc) fallingto 1/7 of the types
 // SNOB'N'US
 
 // PRIMATIVE TYPES (1-6)
