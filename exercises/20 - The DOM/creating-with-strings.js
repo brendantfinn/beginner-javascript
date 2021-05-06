a65fa7b43dc9d10bcfcaed5054c10335bad195b7
brendantfinn <brendantfinn@gmail.com>
2021-05-06 09:46:40 -0500

    dom html from strings and xss completed

---
+++ exercises/20 - The DOM/creating-with-strings.js
@@ -0,0 +1,21 @@
+console.log('it works! creating w/ strings');
+
+const item = document.querySelector('.item');
+
+const width = 500;
+const src = `https://picsum.photos/${width}`;
+const desc = 'Cute Pup';
+const myHTML = `
+<div class="wrapper">
+    <h2>Cute ${desc}</h2>
+    <img src="${src}" alt="${desc}"/>
+</div>`;
+
+// turn string into a DOM element
+
+const myFragment = document.createRange().createContextualFragment(myHTML);
+
+console.log(myFragment.querySelector('img'));
+console.log(myFragment);
+
+document.body.appendChild(myFragment);
