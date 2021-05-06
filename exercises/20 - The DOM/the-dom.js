d6671a0d5abf877547e462de2219d7a2a01f3218
brendantfinn <brendantfinn@gmail.com>
2021-05-06 08:09:51 -0500

    dom working with classes completed

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,14 +1,25 @@
-const p = document.querySelector('p');
-const divs = document.querySelectorAll('div');
-//   NODE LIST IS NOT A DIV - All methods are not built into it
-const heading = document.querySelector('h2');
+// const p = document.querySelector('p');
+// const divs = document.querySelectorAll('div');
+// //   NODE LIST IS NOT A DIV - All methods are not built into it
+// const heading = document.querySelector('h2');
 
-console.log(heading.textContent);
-// textContent is the newer & gets all contetns of HIDDEN(styling etc)
-console.log(heading.innerText);
+// console.log(heading.textContent);
+// // textContent is the newer & gets all contetns of HIDDEN(styling etc)
+// console.log(heading.innerText);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
 
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
-pizzaList.insertAdjacentText('afterbegin', '🏓 ');
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+// pizzaList.insertAdjacentText('afterbegin', '🏓 ');
+
+// CLASSES
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+pic.classList.toggle('round');
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+pic.addEventListener('click', toggleRound);
