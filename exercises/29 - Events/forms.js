be4157579b0c69263db773197303a7c0df9c1a11
brendantfinn <brendantfinn@gmail.com>
2021-05-07 12:51:43 -0500

    events prevent default and form events completed

---
+++ exercises/29 - Events/forms.js
@@ -0,0 +1,35 @@
+console.log('forms work');
+
+const wes = document.querySelector('.wes');
+
+function handleWesClick(e) {
+  console.log(e);
+  e.preventDefault();
+  const shouldChangePage = confirm(
+    'This site might be malicious, do you wish to proceed?'
+  );
+
+  if (shouldChangePage) {
+    window.location = e.currentTarget.href;
+  }
+}
+
+wes.addEventListener('click', handleWesClick);
+
+const signupForm = document.querySelector('[name="signup"]');
+
+signupForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+//   console.dir(e.currentTarget.name.value);
+  const name = e.currentTarget.name.value;
+  if (name.includes('chad')) {
+    alert('sorry bro');
+    e.preventDefault();
+  }
+});
+
+function logEvent(e){
+    console.log(e.currentTarget.value)
+}
+signupForm.name.addEventListener('keyup', logEvent);
+signupForm.name.addEventListener('keydown', logEvent);
